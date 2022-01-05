677b3e2f8161c27cc1c9a954896c3edb91fff2bf
bharat_patil <patilbharat09@gmail.com>
2022-01-06 00:19:10 +0530

    Added scss setup

--- src/app/module/login/login.module.ts
+++ src/app/module/login/login.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../../shared.module';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
 @NgModule({
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [CommonModule, SharedModule, LoginRoutingModule],
   declarations: [LoginComponent],
 })
 export class LoginModule {}
